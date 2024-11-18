e5dfcfe5c4661dd2b48df2b1a0db0d4e98a7aa9d
Rostyk-01tm <rostyslav.borovets.pz.2023@lpnu.ua>
2024-11-18 20:43:08 +0200

    Add Rostyslav Borovets Card

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/Borovets.jpg"} alt="Rostyslav Borovets" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Rostyslav Borovets</h2>
+          <p style={styles.position}>Buisnees analytics</p>
         </div>
       </div>
     </main>
