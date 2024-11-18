c6fec18f86354fb52548b958116ecd0eabe9f5b4
AnastasiiaAdamovych <nastya.adamovych@gmail.com>
2024-11-18 19:34:17 +0200

    Add Anastasiia Adamovych Card

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/anastasiia_avatar.jpg"} alt="Anastasiia Adamovych" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Anastasiia Adamovych</h2>
+          <p style={styles.position}>Project manager</p>
         </div>
       </div>
     </main>
