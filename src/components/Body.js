959da814d661ce6dc001c9375c84153d23c4ef54
Bohdan Hamela (TheThing) <getobodya@gmail.com>
2024-11-20 10:53:42 +0200

    Add Test Card

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/avatar-default.png"} alt="TEST" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>TEST</h2>
+          <p style={styles.position}>TEST</p>
         </div>
       </div>
     </main>
